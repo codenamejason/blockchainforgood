94a0a78c4b13d28f4fb3bfdfd6ad9860032cbb33
codenamejason <codenamejason@outlook.com>
2020-04-18 00:34:20 -0400

    add truffle script to test uniswap exchage w/ dai

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
+let appearingLink = document.getElementById('click-here')
+
+
+
+function ClickHere (props) {
+  //let time = setTimeout(function(){ appearingLink.style }, 3000);
+  
+
+  return(
+    <div>
+      <a
+        className='App-link'
+        id='click-here' 
+        style={{ fontSize: '12px', cursor: 'pointer' }}
+        href='/'  
+      >Click Here To Enter</a>
+    </div>
+  )
+  
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Patient Profile Collector
+        <ClickHere />
       </header>
+      
     </div>
   );
 }
